0c77103bc65c2de168828121da58a3995d9148f9
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2022-09-16 06:12:28 -0400

    feat: :lipstick: GameCard component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,11 +4,14 @@ import { Image, View } from 'react-native';
 import { styles } from './styles';
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from '../../components/Heading';
+import { GAMES } from '../../utils/games'
+import { GameCard } from '../../components/GameCard';
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
       <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..." />
+      <GameCard data={GAMES[0]} />
     </View>
   );
 }
